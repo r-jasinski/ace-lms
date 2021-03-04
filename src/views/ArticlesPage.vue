b962c1e5933af4808728a85d6628aedd3cbe4e60
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-04 13:46:55 -0300

    feat(views): create main views

---
+++ src/views/ArticlesPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="articles">
+    <articles />
+  </div>
+</template>
+
+<script>
+import Articles from '@/components/Articles.vue'
+
+export default {
+  name: 'ArticlesPage',
+  components: {
+    Articles
+  }
+}
+</script>
