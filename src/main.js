058136759995e754bbb59d9e5e6d43c6345ca1fe
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-05 14:59:28 -0300

    feat(main): set up fortawesome dependencies

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+library.add(faLightbulb)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
