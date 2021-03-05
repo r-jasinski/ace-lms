c8789cf85630361b98fd088507af1efaff89cdd3
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-05 11:45:54 -0300

    feat(components): create main header component

---
+++ src/components/layout/MainHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header" />
+</template>
+
+<script>
+export default {
+  name: 'MainHeader'
+}
+</script>
+
+<style scoped>
+.header {
+  height: 40vh;
+  background-image: url('../../assets/bg-header.svg');
+  background-position: center top;
+  background-size: cover;
+}
+</style>
