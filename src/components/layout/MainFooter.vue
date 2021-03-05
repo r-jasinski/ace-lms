289a68de8484fb2aeff833d71b79bdb8ff63419e
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-05 11:43:51 -0300

    feat(components): create main footer component

---
+++ src/components/layout/MainFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="footer">
+    <small>© {{ new Date().getFullYear() }} Copyright: Roberto Jasinski</small>
+    <small>roberto.jasinski@gmail.com</small>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainFooter'
+}
+</script>
+
+<style scoped>
+.footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 15vh;
+  background-image: url('../../assets/bg-footer.svg');
+  background-position: center top;
+  background-size: cover;
+  opacity: 0.8;
+}
+
+small {
+  align-self: center;
+}
+</style>
