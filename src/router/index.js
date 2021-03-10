a429a28906ca48a6a961c5cb522edad99396c830
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-10 15:38:51 -0300

    feat(router index): manage the pages title dynamically

--- src/router/index.js
+++ src/router/index.js
@@ -8,37 +8,55 @@ const routes = [
     path: '/home',
     name: 'HomePage',
     component: () =>
-      import(/* webpackChunkName: "home" */ '../views/HomePage.vue')
+      import(/* webpackChunkName: "home" */ '../views/HomePage.vue'),
+    meta: {
+      title: 'Home'
+    }
   },
   {
     path: '/ranking',
     name: 'RankingPage',
     component: () =>
-      import(/* webpackChunkName: "ranking" */ '../views/RankingPage.vue')
+      import(/* webpackChunkName: "ranking" */ '../views/RankingPage.vue'),
+    meta: {
+      title: 'Ranking'
+    }
   },
   {
     path: '/articles',
     name: 'ArticlesPage',
     component: () =>
-      import(/* webpackChunkName: "articles" */ '../views/ArticlesPage.vue')
+      import(/* webpackChunkName: "articles" */ '../views/ArticlesPage.vue'),
+    meta: {
+      title: 'Artigos'
+    }
   },
   {
     path: '/questions',
     name: 'QuestionsPage',
     component: () =>
-      import(/* webpackChunkName: "questions" */ '../views/QuestionsPage.vue')
+      import(/* webpackChunkName: "questions" */ '../views/QuestionsPage.vue'),
+    meta: {
+      title: 'Perguntas'
+    }
   },
   {
     path: '/profile',
     name: 'ProfilePage',
     component: () =>
-      import(/* webpackChunkName: "profile" */ '../views/ProfilePage.vue')
+      import(/* webpackChunkName: "profile" */ '../views/ProfilePage.vue'),
+    meta: {
+      title: 'Perfil'
+    }
   },
   {
     path: '/about',
     name: 'AboutPage',
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutPage.vue')
+      import(/* webpackChunkName: "about" */ '../views/AboutPage.vue'),
+    meta: {
+      title: ''
+    }
   }
 ]
 
@@ -46,6 +64,14 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+const DEFAULT_TITLE = 'LMS'
+
+router.afterEach(to => {
+  Vue.nextTick(() => {
+    document.title = `ACE: ${to.meta.title || DEFAULT_TITLE}`
+  })
 })
 
 export default router
