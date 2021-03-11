848c21bd761deabb1a4153523a853ad80c04d911
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-11 16:12:42 -0300

    feat(articles): include article link component into articles

--- src/components/articles/Articles.vue
+++ src/components/articles/Articles.vue
@@ -1,111 +1,131 @@
 <template>
-  <div class="app-articles">
-    <h1>Articles</h1>
-    <router-link to="/ranking">Novo artigo</router-link>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, corporis.
-    Sit saepe consequuntur quidem minima dolor, aut magnam. Autem molestiae
-    placeat facere voluptates aspernatur at alias blanditiis molestias incidunt
-    omnis! Illo, numquam. Molestias, at enim excepturi vitae quaerat a
-    doloremque impedit quas minus nulla, error voluptate, perspiciatis totam
-    eum? Autem, vitae amet quae hic laborum sint. Vero, sint ullam. Enim.
-    Quaerat accusamus quis, ut beatae odio eum veniam? Sint ab enim, illo itaque
-    fugit blanditiis, voluptatem, ex labore sapiente magni minima quaerat maxime
-    magnam doloribus consequuntur amet at aliquam eius. Placeat quis numquam
-    fuga velit? Minima corrupti dolore facere perspiciatis. Sint modi assumenda
-    temporibus. Rerum ea cum asperiores explicabo quas ab, placeat magnam
-    dignissimos atque exercitationem ducimus expedita reiciendis praesentium.
-    Nesciunt, consectetur sit commodi dicta modi beatae pariatur ullam veritatis
-    quo neque. Mollitia, reiciendis voluptatum. Explicabo veniam voluptate,
-    illum est commodi eius quas delectus voluptatum earum doloribus ullam
-    obcaecati sint? Voluptas, a libero esse similique minus omnis eligendi,
-    maxime aliquam odit ut fuga, quis eius consequatur reiciendis quod adipisci
-    mollitia exercitationem blanditiis. Vel ipsam corporis aliquid pariatur
-    delectus magnam amet! Labore voluptatem, adipisci cumque quae iure
-    architecto molestias totam blanditiis praesentium enim voluptates voluptate
-    eum expedita magni, tempora esse ea nulla, vero dignissimos! Neque
-    distinctio consequuntur aliquid ea? Reprehenderit, quos. Quos voluptates
-    consectetur ut, assumenda qui perferendis fuga suscipit consequuntur
-    recusandae? Earum impedit officiis blanditiis excepturi nisi optio deserunt
-    doloribus modi pariatur, natus quasi quo ullam asperiores explicabo quam
-    tempore? Consequuntur quibusdam quo similique fugit mollitia est nihil
-    laborum tenetur tempora temporibus dicta adipisci placeat perspiciatis, eos
-    nobis vitae sit eaque. Quos omnis optio ad vero quam nemo unde voluptatem!
-    Explicabo, quas! Ipsam laboriosam sapiente porro temporibus sit ipsa,
-    dolorem laborum eius aliquam commodi voluptates et sequi fuga optio
-    eligendi? Praesentium, ipsa dolores officiis omnis perferendis dolore dicta
-    et voluptatem! Fugit asperiores, necessitatibus exercitationem amet iusto
-    minus, porro eaque consequuntur aperiam facere, nulla nam dolore possimus
-    nostrum nisi nobis eos similique magnam sequi ad quia aliquid odit excepturi
-    assumenda. Harum. Ab doloribus quam, nesciunt doloremque delectus voluptates
-    aspernatur debitis pariatur quo excepturi consequuntur optio sequi, cum
-    repudiandae nam error iste laudantium, earum voluptatibus ducimus inventore
-    maiores sint hic. Soluta, iure. Nesciunt, corporis ab magni officiis
-    laudantium iure dolorem suscipit inventore, saepe autem ex commodi quos sunt
-    adipisci tempora accusantium voluptate totam repellendus enim nihil ut
-    reprehenderit illo aspernatur. Error, necessitatibus? Hic animi unde odio
-    fuga aliquam dolores consequuntur ipsum quaerat enim, est nam veritatis
-    voluptas ut molestiae, sunt rerum et omnis distinctio! Molestiae cupiditate
-    maiores ullam cumque. Adipisci, error facere. Reiciendis quibusdam quae
-    veniam vitae, provident quaerat dolorem quia dignissimos consectetur
-    nostrum, tenetur deleniti minus nemo ea, fugit atque modi eveniet. Dicta,
-    aliquam. Distinctio repudiandae consequuntur, ipsa doloremque perferendis
-    sed? Sequi, qui doloribus libero est placeat asperiores accusamus. Culpa
-    nulla rem ducimus debitis vel reprehenderit ipsa consequatur accusantium,
-    accusamus hic similique perferendis repellendus repellat eligendi,
-    consequuntur ad quis aspernatur beatae. Possimus et ea, temporibus fugiat
-    earum repellat voluptate cumque consequatur est, quibusdam quo, maxime quae.
-    Dolore repellat animi ipsum laborum voluptates facere sequi nostrum harum,
-    labore et, molestiae saepe ut! Eligendi fugit quod autem quam maiores
-    officiis architecto obcaecati placeat repellat. Fugiat autem et rem nisi,
-    itaque ea expedita totam voluptatem perspiciatis nesciunt, delectus laborum
-    fugit placeat voluptas! Repellendus, nesciunt. Eveniet architecto officia
-    ex. Quae consequuntur animi magnam eum quaerat dolorem consectetur eos
-    asperiores modi odit eveniet a provident, vitae maiores temporibus, commodi
-    aliquam illo ut eligendi suscipit reprehenderit. Hic! Consectetur sed
-    ratione perspiciatis deserunt ex quasi dolorum ut voluptates exercitationem,
-    cum natus reprehenderit cumque quos? Velit, ab. Nihil dolorum quas quasi
-    dolore atque! Autem non earum possimus ad omnis. Lorem ipsum dolor sit amet,
-    consectetur adipisicing elit. Ducimus officia consequuntur repellat hic,
-    pariatur aliquam! Corrupti natus earum deserunt unde! Ut sint aliquam nulla
-    perspiciatis et qui voluptas aliquid quibusdam. Natus ut facilis sit
-    explicabo possimus debitis amet voluptatem reiciendis sint est quidem fugit
-    pariatur deserunt quisquam id rem ab iusto reprehenderit dignissimos
-    eligendi commodi, ipsum nulla numquam eos? Eum. Enim incidunt reprehenderit
-    repellat vel quaerat a voluptate, sed dicta adipisci ab in fugit voluptatem
-    quis eligendi! Pariatur, est alias ullam, tempora delectus autem minima, rem
-    distinctio laudantium officiis earum! Perspiciatis repudiandae hic vel
-    necessitatibus sed odio, corrupti omnis sint excepturi, maiores beatae non
-    culpa perferendis doloremque et quia. Repudiandae placeat iusto eveniet,
-    beatae nam mollitia perspiciatis obcaecati facere tenetur. Dolorum qui,
-    dicta eius, ratione autem eum enim voluptatibus accusamus ab, maiores
-    fugiat! At unde neque saepe perspiciatis distinctio natus culpa sapiente
-    quos rem autem, molestiae ipsa aliquid cupiditate in? Accusantium labore
-    excepturi quo deleniti veniam, a, aut hic temporibus, dolores ullam
-    distinctio quasi non nihil cum odio. Beatae a modi adipisci rerum, totam
-    iste fugiat voluptatem pariatur consectetur id. Repellat facilis cum tempore
-    architecto dolorum, quibusdam atque numquam earum maxime eius ipsam, sunt
-    voluptate. Earum asperiores illum quae, pariatur expedita cum ut, voluptatum
-    facilis deserunt vero perspiciatis error sit? Autem adipisci aut voluptatum,
-    explicabo reiciendis fugit maxime iusto sequi animi asperiores sit cumque
-    incidunt ducimus nulla? Dolorem non, ipsa officiis officia aut, perspiciatis
-    iusto ab inventore quo architecto saepe. Nihil aut magni id fuga blanditiis,
-    quam eveniet veritatis quasi adipisci, iure animi est obcaecati expedita
-    atque, libero pariatur? Nulla, nemo? Alias sapiente minus voluptatem
-    molestias laboriosam blanditiis ab nobis? Eius quaerat blanditiis doloribus
-    earum minima, nulla natus veniam sequi animi voluptatum ex sit aspernatur
-    expedita perferendis rem? Esse quas ipsam reiciendis aspernatur animi illum
-    facilis cum incidunt ea explicabo?
+  <div class="articles">
+    <div class="articles__header">
+      Compartilhe seu conhecimento.
+      <hr />
+    </div>
+    <div class="articles__filters">
+      <a href="#">data de postagem</a>
+      <input type="text" placeholder="Pesquisar" />
+      <button class="articles__add-button">+</button>
+    </div>
+    <router-link to="/" v-for="article in articles" :key="article.id">
+      <article-link :article="article" />
+    </router-link>
   </div>
 </template>
 
 <script>
+import ArticleLink from './ArticleLink.vue'
 export default {
-  name: 'Articles'
+  name: 'Articles',
+
+  components: {
+    ArticleLink
+  },
+
+  data() {
+    return {
+      articles: [
+        {
+          title:
+            'O CSS possui um vasto conjunto de propriedades pode receber valores numéricosO CSS possui um vasto conjunto de propriedades pode receber valores numéricos',
+          summary:
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ad natus quae dolorem, obcaecati saepe iusto nisi fugiat eius? Dolore pariatur beatae sequi alias assumenda eos, dolorum consequuntur. Eveniet, et Vero nihil voluptas, pariatur eveniet fugiat sequi ad amet, eius asperiores laudantium dolor molestias voluptatibus qui ab voluptate ipsum esse ea velit tempore veniam error quas magnam. Minus, ut nobis. Dignissimos quibusdam expedita rem vitae iusto quasi odit? Minima molestiae eius adipisci obcaecati ullam debitis quos et quod minus? Necessitatibus, nulla. Aspernatur, veniam distinctio. Explicabo unde animi voluptas. Omnis, quasi'
+        },
+        {
+          title:
+            'O CSS possui um vasto conjunto de propriedades pode receber valores numéricosO CSS possui um vasto conjunto de propriedades pode receber valores numéricos',
+          summary:
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ad natus quae dolorem, obcaecati saepe iusto nisi fugiat eius? Dolore pariatur beatae sequi alias assumenda eos, dolorum consequuntur. Eveniet, et Vero nihil voluptas, pariatur eveniet fugiat sequi ad amet, eius asperiores laudantium dolor molestias voluptatibus qui ab voluptate ipsum esse ea velit tempore veniam error quas magnam. Minus, ut nobis. Dignissimos quibusdam expedita rem vitae iusto quasi odit? Minima molestiae eius adipisci obcaecati ullam debitis quos et quod minus? Necessitatibus, nulla. Aspernatur, veniam distinctio. Explicabo unde animi voluptas. Omnis, quasi'
+        },
+        {
+          title:
+            'O CSS possui um vasto conjunto de propriedades pode receber valores numéricosO CSS possui um vasto conjunto de propriedades pode receber valores numéricos',
+          summary:
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ad natus quae dolorem, obcaecati saepe iusto nisi fugiat eius? Dolore pariatur beatae sequi alias assumenda eos, dolorum consequuntur. Eveniet, et Vero nihil voluptas, pariatur eveniet fugiat sequi ad amet, eius asperiores laudantium dolor molestias voluptatibus qui ab voluptate ipsum esse ea velit tempore veniam error quas magnam. Minus, ut nobis. Dignissimos quibusdam expedita rem vitae iusto quasi odit? Minima molestiae eius adipisci obcaecati ullam debitis quos et quod minus? Necessitatibus, nulla. Aspernatur, veniam distinctio. Explicabo unde animi voluptas. Omnis, quasi'
+        },
+        {
+          title:
+            'O CSS possui um vasto conjunto de propriedades pode receber valores numéricosO CSS possui um vasto conjunto de propriedades pode receber valores numéricos',
+          summary:
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ad natus quae dolorem, obcaecati saepe iusto nisi fugiat eius? Dolore pariatur beatae sequi alias assumenda eos, dolorum consequuntur. Eveniet, et Vero nihil voluptas, pariatur eveniet fugiat sequi ad amet, eius asperiores laudantium dolor molestias voluptatibus qui ab voluptate ipsum esse ea velit tempore veniam error quas magnam. Minus, ut nobis. Dignissimos quibusdam expedita rem vitae iusto quasi odit? Minima molestiae eius adipisci obcaecati ullam debitis quos et quod minus? Necessitatibus, nulla. Aspernatur, veniam distinctio. Explicabo unde animi voluptas. Omnis, quasi'
+        },
+        {
+          title:
+            'O CSS possui um vasto conjunto de propriedades pode receber valores numéricosO CSS possui um vasto conjunto de propriedades pode receber valores numéricos',
+          summary:
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ad natus quae dolorem, obcaecati saepe iusto nisi fugiat eius? Dolore pariatur beatae sequi alias assumenda eos, dolorum consequuntur. Eveniet, et Vero nihil voluptas, pariatur eveniet fugiat sequi ad amet, eius asperiores laudantium dolor molestias voluptatibus qui ab voluptate ipsum esse ea velit tempore veniam error quas magnam. Minus, ut nobis. Dignissimos quibusdam expedita rem vitae iusto quasi odit? Minima molestiae eius adipisci obcaecati ullam debitis quos et quod minus? Necessitatibus, nulla. Aspernatur, veniam distinctio. Explicabo unde animi voluptas. Omnis, quasi'
+        },
+        {
+          title:
+            'O CSS possui um vasto conjunto de propriedades pode receber valores numéricosO CSS possui um vasto conjunto de propriedades pode receber valores numéricos',
+          summary:
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ad natus quae dolorem, obcaecati saepe iusto nisi fugiat eius? Dolore pariatur beatae sequi alias assumenda eos, dolorum consequuntur. Eveniet, et Vero nihil voluptas, pariatur eveniet fugiat sequi ad amet, eius asperiores laudantium dolor molestias voluptatibus qui ab voluptate ipsum esse ea velit tempore veniam error quas magnam. Minus, ut nobis. Dignissimos quibusdam expedita rem vitae iusto quasi odit? Minima molestiae eius adipisci obcaecati ullam debitis quos et quod minus? Necessitatibus, nulla. Aspernatur, veniam distinctio. Explicabo unde animi voluptas. Omnis, quasi'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
-.app-articles {
-  padding: 0 10%;
+.articles {
+  padding: 0 5%;
+}
+
+.articles__header {
+  color: var(--secondary);
+  padding: 0 20%;
+  margin-bottom: 5%;
+  font-size: 1.5em;
+  text-align: center;
+}
+
+.articles__filters {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 40px;
+}
+
+.articles__add-button {
+  color: var(--light);
+  background-color: var(--primary);
+  min-width: 39px;
+  min-height: 36px;
+  padding: 10px;
+  margin-left: 25px;
+  border: none;
+  border-radius: 100vh;
+  outline: none;
+  font-weight: 900;
+}
+
+.articles__add-button:hover {
+  background-color: var(--primary-plus);
+}
+
+input {
+  color: var(--dark);
+  background-color: var(--light);
+  width: 150px;
+  min-height: 36px;
+  border-radius: 18px;
+  border: 2px solid var(--dark);
+  padding: 10px;
+  outline: none;
+}
+
+a {
+  color: var(--dark);
+  margin-right: 20px;
+  text-decoration: none;
+}
+
+hr {
+  width: 25%;
+  border: none;
+  border-top: 1px solid var(--secondary);
 }
 </style>
