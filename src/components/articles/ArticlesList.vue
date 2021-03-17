fb3f30ce337e9b71889288b1d669dfe4a3403b00
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-17 20:57:25 -0300

    feat(articles): standardize articles features

---
+++ src/components/articles/ArticlesList.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="articles-list">
+    <div class="articles-list__filters">
+      <a href="#">data de postagem</a>
+      <filter-input />
+      <add-button :add="addArticle" />
+    </div>
+    <router-link
+      :to="{ name: 'ArticleViewEdit' }"
+      v-for="article in articles"
+      :key="article.id"
+    >
+      <articles-list-link :article="article" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+import AddButton from '@/components/shared/AddButton'
+import FilterInput from '@/components/shared/FilterInput'
+import ArticlesListLink from './ArticlesListLink.vue'
+
+export default {
+  name: 'ArticlesList',
+
+  components: { AddButton, FilterInput, ArticlesListLink },
+
+  data() {
+    return {
+      articles: [
+        {
+          title:
+            '<h1>O CSS possui um vasto conjunto de propriedades pode receber valores numéricosO CSS possui um vasto conjunto de propriedades pode receber valores numéricos</h1>',
+          summary:
+            '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ad natus quae dolorem, obcaecati saepe iusto nisi fugiat eius? Dolore pariatur beatae sequi alias assumenda eos, dolorum consequuntur. Eveniet, et Vero nihil voluptas, pariatur eveniet fugiat sequi ad amet, eius asperiores laudantium dolor molestias voluptatibus qui ab voluptate ipsum esse ea velit tempore veniam error quas magnam. Minus, ut nobis. Dignissimos quibusdam expedita rem vitae iusto quasi odit? Minima molestiae eius adipisci obcaecati ullam debitis quos et quod minus? Necessitatibus, nulla. Aspernatur, veniam distinctio. Explicabo unde animi voluptas. Omnis, quasi</p>',
+          autor: 'anna_nowak',
+          postDate: '05 horas atrás'
+        },
+        {
+          title:
+            '<h1>Em SQL, existe uma variação da instrução SELECT permite que uma outra subconsulta do tipo SELECT seja utilizada como uma coluna da consulta principal</h1>',
+          summary:
+            '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ad natus quae dolorem, obcaecati saepe iusto nisi fugiat eius? Dolore pariatur beatae sequi alias assumenda eos, dolorum consequuntur. Eveniet, et Vero nihil voluptas, pariatur eveniet fugiat sequi ad amet, eius asperiores laudantium dolor molestias voluptatibus qui ab voluptate ipsum esse ea velit tempore veniam error quas magnam. Minus, ut nobis. Dignissimos quibusdam expedita rem vitae iusto quasi odit? Minima molestiae eius adipisci obcaecati ullam debitis quos et quod minus? Necessitatibus, nulla. Aspernatur, veniam distinctio. Explicabo unde animi voluptas. Omnis, quasi</p>',
+          autor: 'jan_kowalski',
+          postDate: '05 minutos atrás'
+        },
+        {
+          title:
+            '<h1>JUnit é uma API de código aberto para a criação de testes unitários em Java, bem como outras linguagem compatíveis com a JVM</h1>',
+          summary:
+            '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ad natus quae dolorem, obcaecati saepe iusto nisi fugiat eius? Dolore pariatur beatae sequi alias assumenda eos, dolorum consequuntur. Eveniet, et Vero nihil voluptas, pariatur eveniet fugiat sequi ad amet, eius asperiores laudantium dolor molestias voluptatibus qui ab voluptate ipsum esse ea velit tempore veniam error quas magnam. Minus, ut nobis. Dignissimos quibusdam expedita rem vitae iusto quasi odit? Minima molestiae eius adipisci obcaecati ullam debitis quos et quod minus? Necessitatibus, nulla. Aspernatur, veniam distinctio. Explicabo unde animi voluptas. Omnis, quasi</p>',
+          autor: 'sam_smith',
+          postDate: '02 dias atrás'
+        },
+        {
+          title: '<h1>Lorem ipsum dolor, sit amet consectetur adipisicing</h1>',
+          summary:
+            '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ad natus quae dolorem, obcaecati saepe iusto nisi fugiat eius? Dolore pariatur beatae sequi alias assumenda eos, dolorum consequuntur. Eveniet, et Vero nihil voluptas, pariatur eveniet fugiat sequi ad amet, eius asperiores laudantium dolor molestias voluptatibus qui ab voluptate ipsum esse ea velit tempore veniam error quas magnam. Minus, ut nobis. Dignissimos quibusdam expedita rem vitae iusto quasi odit? Minima molestiae eius adipisci obcaecati ullam debitis quos et quod minus? Necessitatibus, nulla. Aspernatur, veniam distinctio. Explicabo unde animi voluptas. Omnis, quasi</p>',
+          autor: 'john_doe',
+          postDate: '1 semana atrás'
+        },
+        {
+          title:
+            '<h1>O CSS possui um vasto conjunto de propriedades pode receber valores numéricosO CSS possui um vasto conjunto de propriedades pode receber valores numéricos</h1>',
+          summary:
+            '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ad natus quae dolorem, obcaecati saepe iusto nisi fugiat eius? Dolore pariatur beatae sequi alias assumenda eos, dolorum consequuntur. Eveniet, et Vero nihil voluptas, pariatur eveniet fugiat sequi ad amet, eius asperiores laudantium dolor molestias voluptatibus qui ab voluptate ipsum esse ea velit tempore veniam error quas magnam. Minus, ut nobis. Dignissimos quibusdam expedita rem vitae iusto quasi odit? Minima molestiae eius adipisci obcaecati ullam debitis quos et quod minus? Necessitatibus, nulla. Aspernatur, veniam distinctio. Explicabo unde animi voluptas. Omnis, quasi</p>',
+          autor: 'jan_kowalski',
+          postDate: '05 minutos atrás'
+        },
+        {
+          title:
+            '<h1>O CSS possui um vasto conjunto de propriedades pode receber valores numéricosO CSS possui um vasto conjunto de propriedades pode receber valores numéricos</h1>',
+          summary:
+            '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ad natus quae dolorem, obcaecati saepe iusto nisi fugiat eius? Dolore pariatur beatae sequi alias assumenda eos, dolorum consequuntur. Eveniet, et Vero nihil voluptas, pariatur eveniet fugiat sequi ad amet, eius asperiores laudantium dolor molestias voluptatibus qui ab voluptate ipsum esse ea velit tempore veniam error quas magnam. Minus, ut nobis. Dignissimos quibusdam expedita rem vitae iusto quasi odit? Minima molestiae eius adipisci obcaecati ullam debitis quos et quod minus? Necessitatibus, nulla. Aspernatur, veniam distinctio. Explicabo unde animi voluptas. Omnis, quasi</p>',
+          autor: 'jan_kowalski',
+          postDate: '05 minutos atrás'
+        }
+      ]
+    }
+  },
+
+  methods: {
+    addArticle() {
+      this.$router.push({ name: 'ArticleCreate' })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.articles-list {
+  padding: 0 6%;
+}
+
+.articles-list__filters {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 20px;
+}
+
+.articles-list__add-button {
+  min-width: 39px;
+  min-height: 36px;
+  padding: 10px;
+  color: var(--light);
+  background-color: var(--primary);
+  opacity: 0.75;
+  border: none;
+  border-radius: 100vh;
+  outline: none;
+  font-weight: 900;
+}
+
+.articles-list__add-button:hover {
+  opacity: 1;
+}
+
+.articles-list a {
+  color: var(--dark);
+  text-decoration: none;
+}
+</style>
---
+++ src/components/articles/ArticlesListLink.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="article-list-link">
+    <hr />
+    <p class="article-list-link__title">{{ articleTitle }}</p>
+    <post-info :post-info="article" />
+    <p class="article-list-link__summary">{{ articleText }}</p>
+
+    <hr />
+  </div>
+</template>
+
+<script>
+import PostInfo from '@/components/shared/PostInfo'
+import { htmlToText } from 'html-to-text'
+
+export default {
+  name: 'ArticleLlistLink',
+
+  components: { PostInfo },
+
+  props: {
+    article: { type: Object, required: true }
+  },
+
+  computed: {
+    articleTitle() {
+      return htmlToText(this.article.title, { wordwrap: 130 })
+    },
+    articleText() {
+      return htmlToText(this.article.summary, { wordwrap: 130 })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.article-list-link {
+  padding: 0px 15px;
+}
+
+.article-list-link:hover {
+  border-left: 5px solid var(--primary);
+  border-radius: 9px;
+}
+
+.article-list-link__title {
+  font-size: 1.2em;
+  font-weight: 900;
+}
+
+.article-list-link__summary {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 4;
+  overflow: hidden;
+  text-align: justify;
+  text-justify: inter-word;
+}
+
+.article-list-link hr {
+  border: none;
+  border-top: 1px dotted var(--dark);
+  opacity: 0.2;
+}
+
+@media only screen and (max-width: 768px) {
+  .article-list-link__summary {
+    -webkit-line-clamp: 5;
+  }
+}
+
+@media only screen and (max-width: 480px) {
+  .article-list-link__summary {
+    -webkit-line-clamp: 6;
+  }
+}
+
+@media only screen and (max-width: 320px) {
+  .article-list-link__summary {
+    -webkit-line-clamp: 10;
+  }
+}
+</style>
