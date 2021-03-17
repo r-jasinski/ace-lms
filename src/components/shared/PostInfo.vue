62d3423dcd48f68bf83efae1db3c432c658348e7
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-17 17:45:38 -0300

    feat(post info): create post info component

---
+++ src/components/shared/PostInfo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="post-info">
+    <div class="post-info__avatar" />
+    <ul class="post-info__text">
+      <li>{{ postInfo.autor }}</li>
+      <li>{{ postInfo.postDate }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostInfo',
+
+  props: {
+    postInfo: { type: Object, required: true }
+  }
+}
+</script>
+
+<style scoped>
+.post-info {
+}
+
+.post-info__avatar {
+  float: left;
+  margin-right: 20px;
+  height: 50px;
+  min-width: 50px;
+  background-image: url('https://assets.justinpinkney.com/toonify/images/hd/crops/gosling.jpg');
+  background-position: center;
+  background-size: cover;
+  border-radius: 999px;
+}
+
+.post-info__text {
+  margin-top: 0;
+  list-style: none;
+  margin-bottom: 25px;
+}
+</style>
