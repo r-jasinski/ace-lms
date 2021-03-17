9662fabb4ea3f5b6b63bf7b2447567f951969538
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-17 17:42:35 -0300

    feat(add button): create add button component

---
+++ src/components/shared/AddButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="add-button" @click="add">
+    <font-awesome-icon icon="plus" />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'AddButton',
+
+  props: {
+    add: { type: Function, required: true }
+  }
+}
+</script>
+
+<style scoped>
+.add-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 40px;
+  min-width: 40px;
+  color: #fff;
+  background-color: var(--primary);
+  border-radius: 100vh;
+  border: none;
+  outline: none;
+  font-weight: 900;
+}
+
+.add-button:hover {
+  background-color: var(--primary-plus);
+  box-shadow: var(--primary-plus) 0px 0px 0px 2px;
+}
+</style>
