4682cda37f321dbad35b3e6d2902812aad66abfc
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-17 17:43:58 -0300

    feat(remove button): create remove button component

---
+++ src/components/shared/RemoveButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button class="remove-button" @click="remove">
+    <font-awesome-icon icon="trash" />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'EditButton',
+
+  props: {
+    remove: { type: Function, required: true }
+  }
+}
+</script>
+
+<style scoped>
+.remove-button {
+}
+
+.remove-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 25px 0 50px 0;
+  min-height: 40px;
+  min-width: 40px;
+  color: #fff;
+  background-color: var(--danger);
+  border-radius: 100vh;
+  border: none;
+  outline: none;
+  font-weight: 900;
+}
+
+.remove-button:hover {
+  background-color: var(--danger-plus);
+  box-shadow: var(--danger-plus) 0px 0px 0px 3px;
+}
+</style>
