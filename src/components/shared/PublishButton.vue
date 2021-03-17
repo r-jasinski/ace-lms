1bf1391744891db835a912936c0ead4e7c9daa74
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-17 17:41:32 -0300

    feat(publish button): create publish button component

---
+++ src/components/shared/PublishButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="publish-button" @click="$router.push('/questions')">
+    Publicar
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'PublishButton'
+}
+</script>
+
+<style scoped>
+.publish-button {
+}
+
+.publish-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 25px 0 50px 0;
+  padding: 10px;
+  color: #fff;
+  background-color: var(--primary);
+  border-radius: 100vh;
+  border: none;
+  outline: none;
+  font-weight: 900;
+}
+
+.publish-button:hover {
+  background-color: var(--primary-plus);
+  box-shadow: var(--primary-plus) 0px 0px 0px 2px;
+}
+</style>
