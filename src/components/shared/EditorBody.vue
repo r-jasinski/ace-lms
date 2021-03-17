2b438051b2fe9e97056486e26c7d531b7e41b7b4
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-17 17:47:05 -0300

    feat(editor body): create editor body component

---
+++ src/components/shared/EditorBody.vue
@@ -0,0 +1,217 @@
+<template>
+  <div class="editor" :class="{ 'editor--editable': editable }">
+    <editor-menu-bar
+      v-if="editable"
+      :editor="editor"
+      v-slot="{ commands, isActive }"
+    >
+      <div class="editor__menubar">
+        <button
+          class="editor__button"
+          @click="showImagePrompt(commands.image)"
+          :class="{ 'is-active': isActive.image() }"
+        >
+          <font-awesome-icon icon="image" />
+        </button>
+        <button
+          v-for="({ active, command, icon, level }, index) in menuBarButtons"
+          :key="`${index}-${icon}`"
+          class="editor__button"
+          :class="{
+            'is-active': active && isActive[active]()
+          }"
+          @click="commands[command]({ level })"
+        >
+          <div class="button__icon">
+            <font-awesome-icon :icon="icon" />
+          </div>
+        </button>
+      </div>
+    </editor-menu-bar>
+    <editor-content class="editor__content" :editor="editor" />
+  </div>
+</template>
+
+<script>
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import menuBarButtonsMixin from '@/mixins/editor/menuBarButtonsMixin'
+import {
+  Blockquote,
+  Bold,
+  BulletList,
+  Code,
+  CodeBlock,
+  Focus,
+  HardBreak,
+  Heading,
+  History,
+  HorizontalRule,
+  Image,
+  Italic,
+  Link,
+  ListItem,
+  OrderedList,
+  Placeholder,
+  Strike,
+  TodoItem,
+  TodoList,
+  Underline
+} from 'tiptap-extensions'
+
+export default {
+  name: 'EditorBody',
+
+  components: {
+    EditorContent,
+    EditorMenuBar
+  },
+
+  mixins: [menuBarButtonsMixin],
+
+  props: {
+    content: { type: String, default: '' },
+    editable: { type: Boolean, default: true },
+    placeholder: { type: String, default: '' }
+  },
+
+  data() {
+    return {
+      html: '',
+      editor: new Editor({
+        editable: true,
+        extensions: [
+          new Blockquote(),
+          new Bold(),
+          new BulletList(),
+          new Code(),
+          new CodeBlock(),
+          new Focus({
+            className: 'has-focus',
+            nested: true
+          }),
+          new HardBreak(),
+          new Heading({ levels: [1] }),
+          new History(),
+          new HorizontalRule(),
+          new Image(),
+          new Italic(),
+          new Link(),
+          new ListItem(),
+          new OrderedList(),
+          new Placeholder({
+            emptyEditorClass: 'is-editor-empty',
+            emptyNodeClass: 'is-empty',
+            emptyNodeText: '',
+            showOnlyWhenEditable: true,
+            showOnlyCurrent: true
+          }),
+          new Strike(),
+          new TodoItem(),
+          new TodoList(),
+          new Underline()
+        ],
+        autoFocus: false,
+        content: '',
+        onUpdate: ({ getHTML }) => {
+          this.html = getHTML()
+          this.content = this.html === '<p></p>' ? '' : this.html
+        }
+      })
+    }
+  },
+
+  watch: {
+    editable() {
+      this.editor.setOptions({ editable: this.editable })
+    }
+  },
+
+  mounted() {
+    this.editor.extensions.options.placeholder.emptyNodeText = this.placeholder
+    this.editor.setContent(this.content)
+    this.editor.setOptions({ editable: this.editable })
+  },
+
+  beforeDestroy() {
+    this.editor.destroy()
+  },
+
+  methods: {
+    showImagePrompt() {
+      const src = prompt('Enter the url of your image here')
+      src && this.editor.commands.image({ src })
+    }
+  }
+}
+</script>
+
+<style>
+.editor {
+  border-radius: 18px;
+  border: none;
+  margin-top: 25px;
+  min-height: 10vh;
+  padding: 10px;
+}
+
+.editor--editable {
+  border: 1px solid var(--dark-50);
+}
+
+.editor--editable:focus-within {
+  border: 1px solid var(--dark);
+}
+
+.editor__menubar {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1%;
+}
+
+.editor__button {
+  background-color: rgba(77, 221, 240, 0);
+  border: none;
+  color: var(--dark);
+  height: 35px;
+  opacity: 0.7;
+  outline: none;
+  width: 35px;
+}
+
+.is-active {
+  color: var(--primary);
+  opacity: 0.9;
+}
+
+.editor__button:hover {
+  background-color: rgba(0, 225, 255, 0.082);
+  border-radius: 5px;
+  opacity: 1;
+}
+
+.editor__content blockquote {
+  border-left: 3px solid var(--primary);
+  font-style: italic;
+  opacity: 0.8;
+  padding-left: 0.8rem;
+}
+
+.editor__content pre {
+  background-color: var(--primary);
+  border-radius: 5px;
+  padding: 1%;
+}
+
+.ProseMirror-focused {
+  outline: none;
+}
+
+.editor__content p.is-editor-empty:first-child::before {
+  color: #aaa;
+  content: attr(data-empty-text);
+  float: left;
+  font-style: italic;
+  height: 0;
+  pointer-events: none;
+}
+</style>
