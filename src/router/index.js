cac6a4a49426e79932eee67e71a6a12484f44871
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-23 16:20:56 -0300

    feat(router index): reorganize routes

--- src/router/index.js
+++ src/router/index.js
@@ -6,94 +6,101 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '',
-    name: 'HomePage',
-    component: () => import(/* webpackChunkName: "home" */ '@/views/HomePage'),
-    meta: {
-      title: 'Home'
-    }
-  },
-  {
-    path: '/ranking',
-    name: 'RankingPage',
-    component: () =>
-      import(/* webpackChunkName: "ranking" */ '@/views/RankingPage'),
-    meta: {
-      title: 'Ranking'
-    }
-  },
-  {
-    path: '/articles',
-    component: () =>
-      import(/* webpackChunkName: "articles" */ '@/views/ArticlesPage'),
+    component: () => import('@/views/AppPage'),
     children: [
       {
         path: '',
-        name: 'ArticlesList',
-        component: () => import('@/components/articles/ArticlesList'),
-        meta: { title: 'Artigos' }
+        name: 'HomePage',
+        component: () =>
+          import(/* webpackChunkName: "home" */ '@/views/HomePage'),
+        meta: {
+          title: 'Home'
+        }
       },
       {
-        path: 'view',
-        name: 'ArticleViewEdit',
-        component: () => import('@/components/articles/ArticleViewEdit'),
-        meta: { title: 'Visualizar Artigo' }
+        path: '/ranking',
+        name: 'RankingPage',
+        component: () =>
+          import(/* webpackChunkName: "ranking" */ '@/views/RankingPage'),
+        meta: {
+          title: 'Ranking'
+        }
       },
       {
-        path: 'create',
-        name: 'ArticleCreate',
-        component: () => import('@/components/articles/ArticleCreate'),
-        meta: { title: 'Criar Artigo' }
+        path: '/articles',
+        component: () =>
+          import(/* webpackChunkName: "articles" */ '@/views/ArticlesPage'),
+        children: [
+          {
+            path: '',
+            name: 'ArticlesList',
+            component: () => import('@/components/articles/ArticlesList'),
+            meta: { title: 'Artigos' }
+          },
+          {
+            path: 'view',
+            name: 'ArticleViewEdit',
+            component: () => import('@/components/articles/ArticleViewEdit'),
+            meta: { title: 'Visualizar Artigo' }
+          },
+          {
+            path: 'create',
+            name: 'ArticleCreate',
+            component: () => import('@/components/articles/ArticleCreate'),
+            meta: { title: 'Criar Artigo' }
+          }
+        ]
+      },
+      {
+        path: '/questions',
+        component: () =>
+          import(/* webpackChunkName: "questions" */ '@/views/QuestionsPage'),
+        children: [
+          {
+            path: '',
+            name: 'QuestionsList',
+            component: () => import('@/components/questions/QuestionsList'),
+            meta: { title: 'Perguntas' }
+          },
+          {
+            path: 'view',
+            name: 'QuestionViewEdit',
+            component: () => import('@/components/questions/QuestionViewEdit'),
+            meta: { title: 'Visualizar Pergunta' }
+          },
+          {
+            path: 'create',
+            name: 'QuestionCreate',
+            component: () => import('@/components/questions/QuestionCreate'),
+            meta: { title: 'Criar Pergunta' }
+          }
+        ]
+      },
+      {
+        path: '/profile',
+        name: 'ProfilePage',
+        component: () =>
+          import(/* webpackChunkName: "profile" */ '@/views/ProfilePage'),
+        meta: {
+          title: 'Perfil'
+        }
+      },
+      {
+        path: '/about',
+        name: 'AboutUsersPage',
+        component: () =>
+          import(/* webpackChunkName: "about" */ '@/views/AboutUsersPage'),
+        meta: {
+          title: 'Sobre'
+        }
       }
     ]
-  },
-  {
-    path: '/questions',
-    component: () =>
-      import(/* webpackChunkName: "questions" */ '@/views/QuestionsPage'),
-    children: [
-      {
-        path: '',
-        name: 'QuestionsList',
-        component: () => import('@/components/questions/QuestionsList'),
-        meta: { title: 'Perguntas' }
-      },
-      {
-        path: 'view',
-        name: 'QuestionViewEdit',
-        component: () => import('@/components/questions/QuestionViewEdit'),
-        meta: { title: 'Visualizar Pergunta' }
-      },
-      {
-        path: 'create',
-        name: 'QuestionCreate',
-        component: () => import('@/components/questions/QuestionCreate'),
-        meta: { title: 'Criar Pergunta' }
-      }
-    ]
-  },
-  {
-    path: '/profile',
-    name: 'ProfilePage',
-    component: () =>
-      import(/* webpackChunkName: "profile" */ '@/views/ProfilePage'),
-    meta: {
-      title: 'Perfil'
-    }
-  },
-  {
-    path: '/about',
-    name: 'AboutUsersPage',
-    component: () =>
-      import(/* webpackChunkName: "about" */ '@/views/AboutUsersPage'),
-    meta: {
-      title: 'Sobre'
-    }
   },
   {
     path: '/login',
     name: 'LoginPage',
     component: () =>
-      import(/* webpackChunkName: "about" */ '@/views/LoginPage'),
+      import(/* webpackChunkName: "login" */ '@/views/LoginPage'),
     meta: {
       title: 'Login'
     }
