e1830771351775963bde29f7085e2061052938b8
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-23 16:22:38 -0300

    feat(login page): create login page

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login-page">
+    <login />
+  </div>
+</template>
+
+<script>
+import Login from '@/components/login/Login'
+
+export default {
+  name: 'LoginPage',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style scoped>
+.login-page {
+  width: 100vw;
+  min-width: 320px;
+}
+</style>
