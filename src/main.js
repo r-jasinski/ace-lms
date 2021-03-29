fd80d95286b007ac27e16684d00f35f079a520ba
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-03-29 17:56:31 -0300

    feat(main): set up firebaseConfig

--- src/main.js
+++ src/main.js
@@ -28,6 +28,8 @@ import {
   faWindowMinimize
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -66,8 +68,23 @@ library.add(
 )
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_API_ID
+}
+firebase.initializeApp(firebaseConfig)
+
+let app
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
