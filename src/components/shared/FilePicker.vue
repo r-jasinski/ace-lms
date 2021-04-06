ead27d411e6d69f8d0569a158212e976c9139538
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-04-06 20:05:04 -0300

    feat(file picker): create a file picker component

---
+++ src/components/shared/FilePicker.vue
@@ -0,0 +1,64 @@
+<template>
+  <label class="file-picker">
+    <font-awesome-icon class="file-picker__icon" :icon="icon" />
+    <div class="file-picker__button">
+      <span>{{ (value && value.name) || placeholder }}</span>
+    </div>
+    <input
+      type="file"
+      name="avatar"
+      accept="image/png, image/jpeg"
+      @change="handleFileChange"
+    />
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'FilePicker',
+
+  props: {
+    icon: { type: String, default: 'meh-blank' },
+    placeholder: { type: String, default: '' },
+    type: { type: String, default: 'text' },
+    value: { type: File, default: null }
+  },
+
+  data() {
+    return {}
+  },
+
+  methods: {
+    handleFileChange(e) {
+      this.$emit('input', e.target.files[0])
+    }
+  }
+}
+</script>
+
+<style scoped>
+.file-picker {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  min-height: 50px;
+  color: var(--dark);
+  background-color: transparent;
+  border-radius: 100vh;
+  border: 1px solid var(--dark-50);
+  outline: none;
+}
+
+.file-picker:hover {
+  border: 1px solid var(--dark);
+}
+
+.file-picker__icon {
+  margin: 0 9px 0 18px;
+  opacity: 0.5;
+}
+
+.file-picker > input[type='file'] {
+  display: none;
+}
+</style>
