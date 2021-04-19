ffb8573be04f203aa66ea8fea519877261b04bae
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-04-19 13:58:26 -0300

    feat(reactivate button): create a reactivate button component

---
+++ src/components/shared/ReactivateButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button class="reactivate-button" @click.prevent="$emit('clicked')">
+    <font-awesome-icon icon="redo" />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ReactivateButton'
+}
+</script>
+
+<style scoped>
+.reactivate-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 50px;
+  min-height: 50px;
+  color: #fff;
+  background-color: var(--success);
+  border-radius: 100vh;
+  border: none;
+  outline: none;
+  font-weight: 900;
+  transform: scale(1);
+  transition: all 0.1s;
+}
+
+.reactivate-button:hover {
+  background-color: var(--success-plus);
+  cursor: pointer;
+  transform: scale(1.05);
+  transition: all 0.1s;
+}
+
+.reactivate-button--active {
+  background-color: var(--success-plus);
+  transform: scale(1.05);
+  transition: all 0.1s;
+}
+</style>
