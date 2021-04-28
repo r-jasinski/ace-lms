2e5e5ef92fdce479242e47471deeb4d7e1b44ae8
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-04-27 21:06:04 -0300

    feat(question view and edit): remove hardcoded data and include firestore dynamic data

--- src/components/questions/QuestionViewEdit.vue
+++ src/components/questions/QuestionViewEdit.vue
@@ -6,31 +6,34 @@
         :placeholder="editorTitlePlaceholder"
         :content="question.title"
         :editable="editable"
+        @input="question.title = $event"
       />
       <small v-if="editable" class="question-view-edit__label"
         >O título aparecerá na listagem inicial, portanto seja claro e
         objetivo</small
       >
     </div>
-    <post-info :post-info="question" />
+    <post-info :post-info="postInfo" />
     <editor-body
       :placeholder="editorBodyPlaceholder"
-      :content="question.text"
+      :content="question.content"
       :editable="editable"
+      @input="question.content = $event"
     />
     <div v-if="!editable" class="question-view-edit__buttons">
-      <edit-button @clicked="editQuestion" />
-      <remove-button :remove="removeQuestion" />
+      <edit-button @clicked="editable = true" />
+      <remove-button @clicked="removeQuestion" />
+      <back-button @clicked="$router.go(-1)" />
     </div>
     <div v-else>
       <small class="question-view-edit__label">
         *Ao clicar em “Publicar”, você concorda com os termos de serviço,
         política de privacidade e política de Cookies</small
       >
-      <confirm-button
-        class="question-view-edit__confirm-button"
-        :label="'Publicar'"
-      />
+      <div class="question-view-edit__buttons">
+        <confirm-button :label="'Publicar'" @clicked="updateQuestion" />
+        <cancel-button :label="'Cancelar'" @clicked="cancelEdit" />
+      </div>
     </div>
     <hr />
     Respostas:
@@ -39,26 +42,34 @@
 </template>
 
 <script>
+import BackButton from '@/components/shared/BackButton'
+import CancelButton from '@/components/shared/CancelButton'
+import ConfirmButton from '@/components/shared/ConfirmButton'
+import EditButton from '@/components/shared/EditButton'
 import EditorBody from '@/components/shared/EditorBody'
 import EditorTitle from '../shared/EditorTitle.vue'
 import PostInfo from '@/components/shared/PostInfo'
-import ConfirmButton from '@/components/shared/ConfirmButton'
 import RemoveButton from '@/components/shared/RemoveButton'
-import EditButton from '@/components/shared/EditButton'
 import { htmlToText } from 'html-to-text'
-import { mapActions } from 'vuex'
-import { questionsCollection } from '@/services/questionsService'
+import { mapActions, mapGetters } from 'vuex'
+import {
+  deleteQuestion,
+  questionsCollection,
+  updateQuestion
+} from '@/services/questionsService'
 
 export default {
   name: 'QuestionViewEdit',
 
   components: {
-    RemoveButton,
+    BackButton,
+    CancelButton,
+    ConfirmButton,
     EditButton,
     EditorBody,
     EditorTitle,
     PostInfo,
-    ConfirmButton
+    RemoveButton
   },
 
   data() {
@@ -68,6 +79,22 @@ export default {
       editorBodyPlaceholder: 'Escreva aqui o conteúdo da sua pergunta...',
       editorAnswerPlaceholder: 'Escreva aqui a sua resposta...',
       question: {}
+    }
+  },
+
+  computed: {
+    ...mapGetters({
+      authenticatedUser: 'authenticatedUser/authenticatedUser',
+      user: 'users/user'
+    }),
+
+    postInfo() {
+      let user = this.user(this.question.author)
+      let creationTime = this.question.creationTime
+      return {
+        ...user,
+        creationTime
+      }
     }
   },
 
@@ -96,12 +123,30 @@ export default {
       commitDocumentTitle: 'documentTitle/commitDocumentTitle'
     }),
 
-    removeQuestion() {
-      this.question = {}
+    async removeQuestion() {
+      await deleteQuestion(this.question.id)
+      console.log('removed')
+      this.$router.push({ name: 'QuestionsList' })
     },
 
-    editQuestion() {
-      this.editable = true
+    cancelEdit() {
+      this.editable = false
+      this.question.content += ' '
+    },
+
+    async updateQuestion() {
+      if (
+        this.question.content.length < 10 ||
+        this.question.title.length < 10
+      ) {
+        return
+      }
+      console.log(this.question.title)
+      const UTCStringCreationTime = new Date().toUTCString()
+      this.question.author = this.authenticatedUser.uid
+      this.question.creationTime = UTCStringCreationTime
+      await updateQuestion(this.question.id, this.question)
+      this.editable = false
     },
 
     initializeQuestion() {
@@ -109,6 +154,7 @@ export default {
         .doc(this.$route.params.id)
         .onSnapshot(doc => {
           this.question = { ...doc.data() }
+          this.question.id = doc.id
         })
     }
   }
@@ -133,11 +179,8 @@ export default {
   display: flex;
   align-items: baseline;
   justify-content: flex-end;
+  margin: 50px 0;
   gap: 10px;
-}
-
-.question-view-edit__confirm-button {
-  margin-top: 25px;
 }
 
 .question-view-edit hr {
