6f7d12077b0b55aef8532ffcb167bc8291957602
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-05-18 19:49:21 -0300

    feat(miscellaneous store module): include showTopMenu into state

--- src/store/modules/miscellaneous.js
+++ src/store/modules/miscellaneous.js
@@ -3,19 +3,22 @@ export default {
 
   state: {
     showScrollPercentage: false,
-    articleFontSize: 1
+    articleFontSize: 1,
+    showTopMenu: false
   },
 
   getters: {
     showScrollPercentage: state => state.showScrollPercentage,
-    articleFontSize: state => state.articleFontSize
+    articleFontSize: state => state.articleFontSize,
+    showTopMenu: state => state.showTopMenu
   },
 
   mutations: {
     setShowScrollPercentage: (state, payload) => {
       state.showScrollPercentage = payload
     },
-    setArticleFontSize: (state, payload) => (state.articleFontSize = payload)
+    setArticleFontSize: (state, payload) => (state.articleFontSize = payload),
+    setShowTopMenu: (state, payload) => (state.showTopMenu = payload)
   },
 
   actions: {
@@ -31,6 +34,9 @@ export default {
       if (state.articleFontSize > 0.8) {
         commit('setArticleFontSize', (state.articleFontSize -= 0.1))
       }
+    },
+    commitShowTopMenu: ({ commit }, payload) => {
+      commit('setShowTopMenu', payload)
     }
   }
 }
