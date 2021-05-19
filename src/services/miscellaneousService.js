1fa20fb28a9824187e614688b413289c5e67d732
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-05-19 18:20:21 -0300

    feat(miscellaneous service): create default dialog options

--- src/services/miscellaneousService.js
+++ src/services/miscellaneousService.js
@@ -15,3 +15,18 @@ export const defaultToastOptions = {
   icon: true,
   rtl: false
 }
+
+export const defaultDialogOptions = {
+  html: true, // set to true if your message contains HTML tags. eg: "Delete <b>Foo</b> ?"
+  loader: false, // set to true if you want the dailog to show a loader after click on "proceed"
+  reverse: false, // switch the button positions (left to right, and vise versa)
+  okText: 'Confirmar',
+  cancelText: 'Cancelar',
+  animation: 'bounce', // Available: "zoom", "bounce", "fade"
+  type: 'basic', // coming soon: 'soft', 'hard'
+  verification: 'soft', // for hard confirm, user will be prompted to type this to enable the proceed button
+  verificationHelp: 'Type "[+:verification]" below to confirm', // Verification help text. [+:verification] will be matched with 'options.verification' (i.e 'Type "continue" below to confirm')
+  clicksCount: 3, // for soft confirm, user will be asked to click on "proceed" btn 3 times before actually proceeding
+  backdropClose: true, // set to true to close the dialog when clicking outside of the dialog window, i.e. click landing on the mask
+  customClass: '' // Custom class to be injected into the parent node for the current dialog instance
+}
