0c9c0c6192811f73030ea5653fa9a1657c15f935
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-05-24 18:47:02 -0300

    feat(miscellaneous store module): add isEndOfScroll into state

--- src/store/modules/miscellaneous.js
+++ src/store/modules/miscellaneous.js
@@ -2,33 +2,39 @@ export default {
   namespaced: true,
 
   state: {
-    showScrollPercentage: false,
     articleFontSize: 1,
+    isEndOfScroll: false,
+    showScrollPercentage: false,
     showTopMenu: false
   },
 
   getters: {
-    showScrollPercentage: state => state.showScrollPercentage,
     articleFontSize: state => state.articleFontSize,
+    isEndOfScroll: state => state.isEndOfScroll,
+    showScrollPercentage: state => state.showScrollPercentage,
     showTopMenu: state => state.showTopMenu
   },
 
   mutations: {
+    setArticleFontSize: (state, payload) => (state.articleFontSize = payload),
+    setIsEndOfScroll: (state, payload) => (state.isEndOfScroll = payload),
     setShowScrollPercentage: (state, payload) => {
       state.showScrollPercentage = payload
     },
-    setArticleFontSize: (state, payload) => (state.articleFontSize = payload),
     setShowTopMenu: (state, payload) => (state.showTopMenu = payload)
   },
 
   actions: {
-    commitShowScrollPercentage: ({ commit }, payload) => {
-      commit('setShowScrollPercentage', payload)
-    },
     commitArticleFontSizeIncrease: ({ commit, state }) => {
       if (state.articleFontSize < 2) {
         commit('setArticleFontSize', (state.articleFontSize += 0.1))
       }
+    },
+    commitIsEndOfScroll: ({ commit }, payload) => {
+      commit('setIsEndOfScroll', payload)
+    },
+    commitShowScrollPercentage: ({ commit }, payload) => {
+      commit('setShowScrollPercentage', payload)
     },
     commitArticleFontSizeDecrease: ({ commit, state }) => {
       if (state.articleFontSize > 0.8) {
