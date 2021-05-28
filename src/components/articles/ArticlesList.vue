f09f21c0b952ded78c7434457b6982c2d74943ce
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-05-28 11:59:31 -0300

    feat(articles list): add articles filter and sorting

--- src/components/articles/ArticlesList.vue
+++ src/components/articles/ArticlesList.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="articles-list">
     <div class="articles-list__filters">
-      <a href="#">data de postagem</a>
-      <filter-input />
+      <sort-button
+        label="likes"
+        :isSortedUp="isLikesCountAscending"
+        :disabled="disabled"
+        @clicked="handleLikesCountSorting"
+      />
+      <sort-button
+        label="data de postagem"
+        :isSortedUp="isCreationTimeAscending"
+        :disabled="disabled"
+        @clicked="handleCreationTimeSorting"
+      />
+      <filter-input v-model="filter" />
       <add-button @clicked="addArticle" />
     </div>
     <router-link
@@ -16,8 +27,8 @@
       <articles-list-link :article="article" />
     </router-link>
     <div class="articles-list__loader">
-      <small v-if="noMoreArticles && !loading"
-        >Não há mais artigos publicados. :(
+      <small v-if="noMoreArticles.showMessage && !loading"
+        >{{ noMoreArticles.message }}
         <p @click="addArticle">Que tal publicar o seu!?</p>
       </small>
       <dot-spinner :size="'40px'" :opacity="0.5" v-if="loading" />
@@ -29,35 +40,64 @@
 import AddButton from '@/components/shared/AddButton'
 import ArticlesListLink from './ArticlesListLink.vue'
 import FilterInput from '@/components/shared/FilterInput'
-import { getArticles, getNextArticles } from '@/services/articlesService'
+import SortButton from '@/components/shared/SortButton.vue'
+import {
+  filterArticlesBy,
+  getArticles,
+  getNextArticles
+} from '@/services/articlesService'
 import { mapGetters } from 'vuex'
 import { slugify } from '@/services/slugsService'
 
 export default {
   name: 'ArticlesList',
 
-  components: { AddButton, ArticlesListLink, FilterInput },
+  components: { AddButton, ArticlesListLink, FilterInput, SortButton },
 
   data() {
     return {
       articles: [],
+      filter: '',
       itemsPerPage: 4,
       loading: false,
-      noMoreArticles: false
+      noMoreArticles: {
+        showMessage: false,
+        message: ''
+      },
+      orderBy: {
+        field: 'creationTime',
+        asc: false
+      }
     }
   },
 
   computed: {
     ...mapGetters({
       isEndOfScroll: 'miscellaneous/isEndOfScroll'
-    })
+    }),
+
+    disabled() {
+      return !this.articles?.length || !!this.filter
+    },
+
+    isLikesCountAscending() {
+      return this.orderBy.field === 'likesCount' && this.orderBy.asc
+    },
+
+    isCreationTimeAscending() {
+      return this.orderBy.field === 'creationTime' && this.orderBy.asc
+    }
   },
 
   watch: {
     async isEndOfScroll() {
-      if (this.isEndOfScroll) {
+      if (this.isEndOfScroll && !this.filter) {
         this.getNextArticles()
       }
+    },
+
+    async filter() {
+      await this.filterArticlesBy()
     }
   },
 
@@ -70,22 +110,73 @@ export default {
       this.$router.push({ name: 'ArticleCreate' })
     },
 
+    async filterArticlesBy() {
+      if (this.filter.length) {
+        const arrayFilter = this.filter.toLowerCase().split(' ')
+        this.loading = true
+        this.articles = await filterArticlesBy('keywords', arrayFilter)
+        this.loading = false
+        if (this.articles?.length) {
+          this.noMoreArticles = {
+            showMessage: true,
+            message: 'Não há mais artigos com os critérios fornecidos!'
+          }
+          return
+        }
+        this.noMoreArticles = {
+          showMessage: true,
+          message: 'Não foram encontrados artigos com os critérios fornecidos!'
+        }
+        return
+      }
+      await this.initializeArticles()
+    },
+
     async initializeArticles() {
       this.loading = true
-      this.articles = await getArticles('creationTime', this.itemsPerPage)
+      this.articles = await getArticles(
+        this.orderBy.field,
+        this.orderBy.asc ? 'asc' : 'desc',
+        this.itemsPerPage
+      )
       this.loading = false
+      this.noMoreArticles.showMessage = false
     },
 
     async getNextArticles() {
       this.loading = true
-      const moreData = await getNextArticles('creationTime', this.itemsPerPage)
+      const moreData = await getNextArticles(
+        this.orderBy.field,
+        this.orderBy.asc ? 'asc' : 'desc',
+        this.itemsPerPage
+      )
       this.loading = false
       if (!moreData.length) {
-        this.noMoreArticles = true
+        this.noMoreArticles = {
+          showMessage: true,
+          message: 'Não há mais artigos!'
+        }
         return
       }
       this.articles.push(...moreData)
-      this.noMoreArticles = false
+    },
+
+    async handleCreationTimeSorting() {
+      if (!this.articles?.length || !!this.filter) {
+        return
+      }
+      this.orderBy.field = 'creationTime'
+      this.orderBy.asc = !this.orderBy.asc
+      await this.initializeArticles()
+    },
+
+    async handleLikesCountSorting() {
+      if (!this.articles?.length || !!this.filter) {
+        return
+      }
+      this.orderBy.field = 'likesCount'
+      this.orderBy.asc = !this.orderBy.asc
+      await this.initializeArticles()
     },
 
     slugifyArticleTitle(articleTitle) {
