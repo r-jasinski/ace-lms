0a072f4244696199870639ad4e618a5ca3c79e89
Roberto Jasinski <roberto.jasinski@gmail.com>
2021-05-28 11:44:41 -0300

    feat(keyword input): create KeywordInput shared component

---
+++ src/components/shared/KeywordInput.vue
@@ -0,0 +1,85 @@
+<template>
+  <form-group :validator="v" :name="name">
+    <div class="keyword-input">
+      <small class="keyword-input__label"
+        >Você deve adicionar palavras-chave para auxiliar nas buscas por esse
+        artigo</small
+      >
+      <input
+        type="text"
+        placeholder="Escreva aqui suas palavras-chave"
+        class="keyword-input__input"
+        v-model.trim="inputModel"
+        @input="v.$touch()"
+      />
+    </div>
+  </form-group>
+</template>
+
+<script>
+export default {
+  name: 'KeywordInput',
+
+  props: {
+    name: { type: String, default: '' },
+    v: { type: Object, required: true },
+    value: { type: String, default: '' }
+  },
+
+  computed: {
+    inputModel: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.keyword-input {
+  display: flex;
+  column-gap: 5px;
+  flex-direction: column;
+  gap: 5px;
+}
+
+.keyword-input__input {
+  background-color: transparent;
+  border-radius: 1em;
+  border: 1px solid var(--dark-50);
+  color: var(--dark);
+  min-height: 3em;
+  outline: none;
+  padding: 10px;
+}
+
+.keyword-input__input:focus {
+  border: 1px solid var(--dark);
+}
+
+.keyword-input__label {
+  font-size: 0.75em;
+}
+
+::placeholder {
+  color: #aaa;
+  font-style: italic;
+  pointer-events: none;
+}
+
+:-ms-input-placeholder {
+  color: #aaa;
+  font-style: italic;
+  pointer-events: none;
+}
+
+::-ms-input-placeholder {
+  color: #aaa;
+  font-style: italic;
+  pointer-events: none;
+}
+</style>
